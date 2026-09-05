0ec1366883f6d3bb0264623c4ec9ba687b074d75
Vidushi Sharma <vidushisharma411@gmail.com>
2026-09-05 18:19:47 +0530

    Fix production API proxy

--- netlify/functions/restaurants.js
+++ netlify/functions/restaurants.js
@@ -1,12 +1,17 @@
-export default async (req) => {
-  const response = await fetch(
-    "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.9711023&lng=77.6544715&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
-  );
+export default async () => {
+  const swiggyUrl =
+    "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.9711023&lng=77.6544715&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 
-  const data = await response.json();
+  const proxyUrl =
+    "https://api.allorigins.win/raw?url=" +
+    encodeURIComponent(swiggyUrl);
 
-  return new Response(JSON.stringify(data), {
-    status: 200,
+  const response = await fetch(proxyUrl);
+
+  const data = await response.text();
+
+  return new Response(data, {
+    status: response.status,
     headers: {
       "Content-Type": "application/json",
       "Access-Control-Allow-Origin": "*",
